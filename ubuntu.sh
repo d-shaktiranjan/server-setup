fc9b9b8293a436879f3a521f95d29c582fc9219f
Shakti Ranjan Debata <shakti.ranjan@growthpanda.co>
2024-09-02 12:47:40 +0530

    feat: ubuntu: add ssh keys into users's authorized_keys

--- ubuntu.sh
+++ ubuntu.sh
@@ -43,13 +43,6 @@ print_message "Creating .ssh directory"
 mkdir -p /root/.ssh
 chmod 700 /root/.ssh
 
-# Add SSH public keys to authorized_keys
-print_message "Adding SSH public keys to authorized_keys"
-cat << 'EOF' >> /root/.ssh/authorized_keys
-ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOHDV/GXK5SNvVj954rQmYx4f5oicCXWdo0aw1ElkYsR rchoudhury63@gmail.com
-ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFpDBlyev/RfBkOe/AuuX9rW6xW+cgFQq6F/Bp4UaCYl debatashaktiranjan@gmail.com
-EOF
-
 # Ensure correct permissions on authorized_keys
 chmod 600 /root/.ssh/authorized_keys
 
@@ -64,6 +57,13 @@ usermod -aG sudo ubuntu
 # Disable password login for the 'ubuntu' user
 print_message "Disabling password login for 'ubuntu'"
 passwd -d ubuntu  # Removes any existing password
+
+# Add SSH public keys to authorized_keys
+print_message "Adding SSH public keys to authorized_keys"
+cat << 'EOF' >> /home/ubuntu/.ssh/authorized_keys
+ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOHDV/GXK5SNvVj954rQmYx4f5oicCXWdo0aw1ElkYsR rchoudhury63@gmail.com
+ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFpDBlyev/RfBkOe/AuuX9rW6xW+cgFQq6F/Bp4UaCYl debatashaktiranjan@gmail.com
+EOF
 
 # Prompt user to install Node.js
 read -p "Do you want to install Node.js (latest LTS)? [y/n]: " install_node
